26faa301b8d864c35ff7d8eba8979d2574653258
Lcfvs <michael.rouges@gmail.com>
2016-07-07 02:54:41 +0200

    Initial commit

---
+++ classes/Modules/Welcome/Views/Index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Copyright 2015 Lcf.vs
+ -
+Released under the MIT license
+ -
+https://github.com/Lcfvs/DOMArch
+*/
+namespace Modules\Welcome\Views;
+
+use Assemblies\HTML\Sections\Welcome as Section;
+use Lib\View\HTML\Page;
+
+class Index
+    extends Page
+{
+    public function get()
+    {
+        $section = Section::assemble($this);
+
+        $document = $this->getDocument();
+
+        $ul = $document->create([
+            'tag' => 'ul'
+        ]);
+        
+        $section->translate();
+    }
+}
---
+++ cli/cli.php
@@ -0,0 +1,16 @@
+<?php
+chdir(__DIR__ . '/..');
+
+require_once 'vendor/autoload.php';
+
+Lib\Config::parse('config.json', true);
+Lib\Bootstrap\Cli::bootstrap();
+
+if ($argv[0] === 'cli' . DIRECTORY_SEPARATOR . 'cli.php') {
+    (function($argv) {
+        Indoctrinated\Db::run(
+            implode(' ', array_slice($argv, 1)));
+    })($argv);
+
+    exit;
+}
